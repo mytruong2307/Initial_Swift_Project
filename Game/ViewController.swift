6fb3fd9dc8c044641ddb02110a3229f73f7c9075
My Truong <truongvanmy@gmail.com>
2019-06-04 15:15:23 +0700

    Initial Commit

---
+++ Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Mytruong on 6/4/19.
+//  Copyright © 2019 Mytruong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
